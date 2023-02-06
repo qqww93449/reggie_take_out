3a22c6a88fedf6491d9dc65693fe27f6acc9d96a
ccxie <qw1020046@gmail.com>
2023-02-06 10:14:12 +0800

    Redis环境的配置

---
+++ src/main/java/com/xcc/reggie_take_out/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.xcc.reggie_take_out.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author Charles
+ * @version 1.0
+ * @className RedisConfig
+ * Redis配置类
+ */
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        return redisTemplate;
+    }
+
+
+}
