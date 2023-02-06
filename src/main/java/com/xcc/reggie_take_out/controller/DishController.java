35068a11b87bc578d0958a0f181b153aa925ab51
ccxie <qw1020046@gmail.com>
2023-02-06 17:52:02 +0800

    使用Redis缓存短信验证码和菜品数据

--- src/main/java/com/xcc/reggie_take_out/controller/DishController.java
+++ src/main/java/com/xcc/reggie_take_out/controller/DishController.java
@@ -13,9 +13,12 @@ import com.xcc.reggie_take_out.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -33,6 +36,8 @@ import java.util.stream.Collectors;
 @RequestMapping("/dish")
 public class DishController {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
     @Autowired
     private CategoryService categoryService;
     @Autowired
@@ -60,33 +65,33 @@ public class DishController {
     public R<Page> page(int page, int pageSize, String name) {
 
         //构造一个分页构造器对象
-        Page<Dish> dishPage = new Page<>(page,pageSize);
-        Page<DishDto> dishDtoPage = new Page<>(page,pageSize);
+        Page<Dish> dishPage = new Page<>(page, pageSize);
+        Page<DishDto> dishDtoPage = new Page<>(page, pageSize);
         //上面对dish泛型的数据已经赋值了，这里对DishDto我们可以把之前的数据拷贝过来进行赋值
 
         //构造一个条件构造器
         LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
         //添加过滤条件 注意判断是否为空  使用对name的模糊查询
-        queryWrapper.like(name != null,Dish::getName,name);
+        queryWrapper.like(name != null, Dish::getName, name);
         //添加排序条件  根据更新时间降序排
         queryWrapper.orderByDesc(Dish::getUpdateTime);
         //去数据库处理分页 和 查询
-        dishService.page(dishPage,queryWrapper);
+        dishService.page(dishPage, queryWrapper);
 
         //获取到dish的所有数据 records属性是分页插件中表示分页中所有的数据的一个集合
         List<Dish> records = dishPage.getRecords();
 
-        List<DishDto> list = records.stream().map((item) ->{
+        List<DishDto> list = records.stream().map((item) -> {
             //对实体类DishDto进行categoryName的设值
 
             DishDto dishDto = new DishDto();
             //这里的item相当于Dish  对dishDto进行除categoryName属性的拷贝
-            BeanUtils.copyProperties(item,dishDto);
+            BeanUtils.copyProperties(item, dishDto);
             //获取分类的id
             Long categoryId = item.getCategoryId();
             //通过分类id获取分类对象
             Category category = categoryService.getById(categoryId);
-            if ( category != null){
+            if (category != null) {
                 //设置实体类DishDto的categoryName属性值
                 String categoryName = category.getName();
                 dishDto.setCategoryName(categoryName);
@@ -95,7 +100,7 @@ public class DishController {
         }).collect(Collectors.toList());
 
         //对象拷贝  使用框架自带的工具类，第三个参数是不拷贝到属性
-        BeanUtils.copyProperties(dishPage,dishDtoPage,"records");
+        BeanUtils.copyProperties(dishPage, dishDtoPage, "records");
         dishDtoPage.setRecords(list);
         //因为上面处理的数据没有分类的id,这样直接返回R.success(dishPage)虽然不会报错，但是前端展示的时候这个菜品分类这一数据就为空
         //所以进行了上面的一系列操作
@@ -103,7 +108,7 @@ public class DishController {
     }
 
     @GetMapping("/{id}")
-    public R<DishDto> get(@PathVariable Long id){
+    public R<DishDto> get(@PathVariable Long id) {
         DishDto dishDto = dishService.getByIdWithFlavor(id);
         return R.success(dishDto);
     }
@@ -118,6 +123,15 @@ public class DishController {
     public R<String> update(@RequestBody DishDto dishDto) {
         //log.info(dishDto.toString());
 
+        //清理菜品的所有缓存数据
+        //Set keys = redisTemplate.keys("dish_*");
+        //redisTemplate.delete(keys);
+
+        //清理某个分类下面的菜品缓存数据
+        String key = "dish_" + dishDto.getCategoryId() + "_1";
+        redisTemplate.delete(key);
+
+
         dishService.updateWithFlavor(dishDto);
 
         return R.success("新增菜品成功");
@@ -126,27 +140,41 @@ public class DishController {
 
     //方法改造
     @GetMapping("/list")
-    public R<List<DishDto>> list(Dish dish){ //会自动映射的
+    public R<List<DishDto>> list(Dish dish) { //会自动映射的
         //这里可以传categoryId,但是为了代码通用性更强,这里直接使用dish类来接受（因为dish里面是有categoryId的）,以后传dish的其他属性这里也可以使用
+
+        //动态构造key
+        List<DishDto> dishDtoList = null;
+
+        String key = "dish_" + dish.getCategoryId() + "_" + dish.getStatus(); //dish_1397844263642378242_1
+
+        //先从redis中获取缓存数据
+        dishDtoList = (List<DishDto>) redisTemplate.opsForValue().get(key);
+
+        if (dishDtoList != null) {
+            //如果存在，直接返回，无需查数据库
+            return R.success(dishDtoList);
+        }
+
         //构造查询条件
         LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(dish.getCategoryId() != null ,Dish::getCategoryId,dish.getCategoryId());
+        queryWrapper.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
         //添加条件，查询状态为1（起售状态）的菜品
-        queryWrapper.eq(Dish::getStatus,1);
+        queryWrapper.eq(Dish::getStatus, 1);
 
         //添加排序条件
         queryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
         List<Dish> list = dishService.list(queryWrapper);
 
         //进行集合的泛型转化
-        List<DishDto> dishDtoList = list.stream().map((item) ->{
+        dishDtoList = list.stream().map((item) -> {
             DishDto dishDto = new DishDto();
             //为一个新的对象赋值，一定要考虑你为它赋过几个值，否则你自己都不知道就返回了null的数据
             //为dishDto对象的基本属性拷贝
-            BeanUtils.copyProperties(item,dishDto);
+            BeanUtils.copyProperties(item, dishDto);
             Long categoryId = item.getCategoryId();
             Category category = categoryService.getById(categoryId);
-            if (category != null){
+            if (category != null) {
                 String categoryName = category.getName();
                 dishDto.setCategoryName(categoryName);
             }
@@ -155,7 +183,7 @@ public class DishController {
             Long dishId = item.getId();
             //创建条件查询对象
             LambdaQueryWrapper<DishFlavor> lambdaQueryWrapper = new LambdaQueryWrapper();
-            lambdaQueryWrapper.eq(DishFlavor::getDishId,dishId);
+            lambdaQueryWrapper.eq(DishFlavor::getDishId, dishId);
             //select * from dish_flavor where dish_id = ?
             //这里之所以使用list来条件查询那是因为同一个dish_id 可以查出不同的口味出来,就是查询的结果不止一个
             List<DishFlavor> dishFlavorList = dishFlavorService.list(lambdaQueryWrapper);
@@ -164,10 +192,11 @@ public class DishController {
             return dishDto;
         }).collect(Collectors.toList());
 
+        //如果不存在，需要查询数据库，将查询到的菜品数据缓存到redis
+        redisTemplate.opsForValue().set(key, dishDtoList, 60, TimeUnit.MINUTES);
+
         return R.success(dishDtoList);
     }
-
-
 
 
 }
--- src/main/java/com/xcc/reggie_take_out/controller/UserController.java
+++ src/main/java/com/xcc/reggie_take_out/controller/UserController.java
@@ -8,6 +8,8 @@ import com.xcc.reggie_take_out.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Charles
@@ -30,21 +33,29 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @PostMapping("/sendMsg")
-    public R<String> sendMsg(@RequestBody User user, HttpSession session){
+    public R<String> sendMsg(@RequestBody User user, HttpSession session) {
         //获取手机号
         String phone = user.getPhone();
-        if (StringUtils.isNotEmpty(phone)){
+        if (StringUtils.isNotEmpty(phone)) {
             //随机生成的4为验证码
             Integer integerCode = ValidateCodeUtils.generateValidateCode(4);
             String code = integerCode.toString();
 
-            log.info("code={}",code);
+            log.info("code={}", code);
             //调用阿里云提供的短信服务api完成发送短信  这里个人用户申请不了阿里云短信服务的签名，所以这里在后台输出了
             //SMSUtils.sendMessage("","","","");
 
             //把验证码存起来  这里使用session来存放验证码，当然也可以存到redis
-            session.setAttribute(phone,code);
+            //session.setAttribute(phone,code);
+
+            //需要将生成的验证码到Redis中，并且设置有效期为5分钟
+            redisTemplate.opsForValue().set(phone, code, 5, TimeUnit.MINUTES);
+
+
             return R.success("手机验证码发送成功");
         }
 
@@ -53,12 +64,13 @@ public class UserController {
 
     /**
      * 移动端用户登录
+     *
      * @param map
      * @param session
      * @return
      */
     @PostMapping("/login")
-    public R<User> login(@RequestBody Map map, HttpSession session){
+    public R<User> login(@RequestBody Map map, HttpSession session) {
         //log.info(map.toString());
 
         //获取手机号
@@ -68,17 +80,18 @@ public class UserController {
         String code = map.get("code").toString();
 
         //从Session中获取保存的验证码
-        Object codeInSession = session.getAttribute(phone);
+        //Object codeInSession = session.getAttribute(phone);
+        Object codeInSession = redisTemplate.opsForValue().get(phone);
 
         //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
-        if(codeInSession != null && codeInSession.equals(code)){
+        if (codeInSession != null && codeInSession.equals(code)) {
             //如果能够比对成功，说明登录成功
 
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(User::getPhone,phone);
+            queryWrapper.eq(User::getPhone, phone);
             //根据用户的手机号去用户表获取用户
             User user = userService.getOne(queryWrapper);
-            if(user == null){
+            if (user == null) {
                 //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册
                 user = new User();
                 user.setPhone(phone);
@@ -87,7 +100,12 @@ public class UserController {
                 userService.save(user);
             }
             //这一行容易漏。。保存用户登录状态
-            session.setAttribute("user",user.getId()); //在session中保存用户的登录状态,这样才过滤器的时候就不会被拦截了
+            //在session中保存用户的登录状态,这样才过滤器的时候就不会被拦截了
+            session.setAttribute("user", user.getId());
+
+            //如果登录成功，删除验证码
+            redisTemplate.delete(phone);
+
             return R.success(user);
         }
         return R.error("登录失败");
